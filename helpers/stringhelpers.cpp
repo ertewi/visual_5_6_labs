1d71fb86d1b26550e638719e81f34784d84278c5
ertewi <60309968+ertewi@users.noreply.github.com>
2024-11-19 00:25:45 +0700

    Add files via upload

---
+++ helpers/stringhelpers.cpp
@@ -0,0 +1,3 @@
+#include "stringhelpers.h"
+
+StringHelpers::StringHelpers() {}
---
+++ helpers/stringhelpers.h
@@ -0,0 +1,10 @@
+#ifndef STRINGHELPERS_H
+#define STRINGHELPERS_H
+
+class StringHelpers
+{
+  public:
+	StringHelpers();
+};
+
+#endif // STRINGHELPERS_H
